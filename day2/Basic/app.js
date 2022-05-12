22e74a009d59c617fe056b96659e50f66d2fd2f8
codewithdolly <hicoder.dolly@gmail.com>
2022-05-13 00:47:03 +0530

    learning

---
+++ day2/Basic/app.js
@@ -0,0 +1,10 @@
+
+//checking memory
+const os = require('node:os');
+console.log(os.freemem());
+console.log(os.arch());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.uptime());
+
+
