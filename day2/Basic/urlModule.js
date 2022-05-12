90691870adf81793d56c4116598d17da3df2b536
codewithdolly <hicoder.dolly@gmail.com>
2022-05-13 02:42:42 +0530

    creat url with node

--- day2/Basic/urlModule.js
+++ day2/Basic/urlModule.js
@@ -2,6 +2,7 @@ const myURL = new URL('https://example.org');
 myURL.pathname = '/a/b/c';
 myURL.search = '?d=e';
 myURL.hash = '#fgh';
+myURL.port= '5000'  //added port
 
 console.log("url is ", myURL);
 console.log("url hash is ", myURL.href);
